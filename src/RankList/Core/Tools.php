03470d746495b5682cea4cc2e1c633f6fa568507
1171213017 <1171213017@qq.com>
2020-04-22 17:30:36 +0800

    初始版本

---
+++ src/RankList/Core/Tools.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Authoer: Superlong
+ */
+
+ namespace RankList\Core;
+
+ class Tools{
+     public function __construct(){
+
+     }
+
+     public static function Hello(){
+         echo "hello world";
+     }
+ }
